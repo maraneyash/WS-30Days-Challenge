442ec4949bb83f3fae51f19b543adb88e8b1e50a
maraneyash <96945990+maraneyash@users.noreply.github.com>
2023-08-27 20:43:36 +0530

    Day 26: Given a binary tree, find the largest value in each level.

---
+++ LargestValueInEachLevel.java
@@ -0,0 +1,64 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+public class LargestValueInEachLevel {
+    public static List<Integer> largestValues(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            int max = Integer.MIN_VALUE;
+
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode node = queue.poll();
+                max = Math.max(max, node.val);
+
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+
+            result.add(max);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(3);
+        root.right = new TreeNode(2);
+        root.left.left = new TreeNode(5);
+        root.left.right = new TreeNode(3);
+        root.right.right = new TreeNode(9);
+
+        List<Integer> largestValuesList = largestValues(root);
+        System.out.println("Largest value in each level:");
+        for (int value : largestValuesList) {
+            System.out.print(value + " ");
+        }
+    }
+}
